2b2f1a2bf89c296544757f888bcb7aa6bffded1d
hwangej <hij3360@naver.com>
2024-03-15 15:29:30 +0900

    hwangej / API_URL from page -> constants

--- components/movie-info.tsx
+++ components/movie-info.tsx
@@ -1,5 +1,5 @@
+import { API_URL } from "../app/constant";
 import potato from "../styles/movie-info.module.css";
-import { API_URL } from "../app/(home)/page";
 
 export async function getMovie(id: string) {
   console.log(`Fetching movies: ${Date.now()}`);
