2c591d9a870e28cdfee4cceb46ef4ea5b5e262e6
hwangej <hij3360@naver.com>
2024-03-15 15:03:13 +0900

    hwange / 3.5 Deployment

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,6 +1,7 @@
+import { getModifiedCookieValues } from "next/dist/server/web/spec-extension/adapters/request-cookies";
 import { Suspense } from "react";
 import { API_URL } from "../../../(home)/page";
-import MovieInfo from "../../../../components/movie-info";
+import MovieInfo, { getMovie } from "../../../../components/movie-info";
 import MovieVideos from "../../../../components/movie-videos";
 
 //1. function async
@@ -20,23 +21,25 @@ import MovieVideos from "../../../../components/movie-videos";
 //   return response.json();
 // }
 
-export default async function MovieDetail({
-  params: { id },
-}: {
+interface IParams {
   params: { id: string };
-}) {
+}
+
+export async function generateMetadata({ params: { id } }: IParams) {
+  const movie = await getMovie(id);
+  return {
+    title: movie.title,
+  };
+}
+
+export default async function MovieDetailPage({ params: { id } }: IParams) {
   // const [movie, videos] = await Promise.all([getMovie(id), getVideos(id)]);
   return (
     <div>
-      <h3>Movie detail paige</h3>
       <Suspense fallback={<h1>Loading movie info</h1>}>
-        {/* Next.js 서버 컴포넌트를 async로 감싸서 사용하게 되면 발생하는 에러임.
-        React 컴포넌트는 jsx만 반환하는 것으로 알고있는 tyhpescript가 캐치하는에러임
-        따라서 아래 except error를 넣으면 해당코드가 오류로 반환됨 */}
         {/* @ts-expect-error Async Server Component */}
         <MovieInfo id={id} />
       </Suspense>
-      <h4>Videos</h4>
       <Suspense fallback={<h1>Loading movie videos</h1>}>
         {/* @ts-expect-error Async Server Component */}
         <MovieVideos id={id} />
